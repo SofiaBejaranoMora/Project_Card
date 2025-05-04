b428a19444480fce83d805a076aae579c674fb37
AshlyrAMIREZ <ashly.navarro.ramirez@est.una.ac.cr>
2025-05-03 19:05:36 -0600

    Second commit
    Vistas

---
+++ Project_Card/src/main/java/cr/ac/una/project_card/controller/PointsController.java
@@ -0,0 +1,15 @@
+package cr.ac.una.project_card.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/** * FXML Controller class * * @author ashly */
+public class PointsController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
--- Project_Card/src/main/java/module-info.java
+++ Project_Card/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module cr.ac.una.project_card {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.base;
 
     opens cr.ac.una.project_card to javafx.fxml;
     exports cr.ac.una.project_card;

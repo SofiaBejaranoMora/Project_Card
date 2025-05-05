c292d75921df988ab278bed100000a3902f542ee
SofiaBejaranoMora <142953533+SofiaBejaranoMora@users.noreply.github.com>
2025-05-05 17:39:18 -0600

    se agrego al modulo material fx

--- Project_Card/src/main/java/cr/ac/una/project_card/controller/MenuController.java
+++ Project_Card/src/main/java/cr/ac/una/project_card/controller/MenuController.java
@@ -1,15 +1,59 @@
 package cr.ac.una.project_card.controller;
 
+import io.github.palexdev.materialfx.controls.MFXButton;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
 
 /** * FXML Controller class * * @author ashly */
 public class MenuController implements Initializable {
+
+    @FXML
+    private MFXButton btnSettings;
+    @FXML
+    private MFXButton btnNew;
+    @FXML
+    private MFXButton btnContinue;
+    @FXML
+    private MFXButton btnAchievements;
+    @FXML
+    private MFXButton btnCloseSession;
+    @FXML
+    private Button btnSession;
+    @FXML
+    private ImageView mgvUserPorfile;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void onActionBtnSettings(ActionEvent event) {
+    }
+
+    @FXML
+    private void onActionBtnNew(ActionEvent event) {
+    }
+
+    @FXML
+    private void onActionBtnContinue(ActionEvent event) {
+    }
+
+    @FXML
+    private void onActionBtnAchievements(ActionEvent event) {
+    }
+
+    @FXML
+    private void OnActionBtnCloseSession(ActionEvent event) {
+    }
+
+    @FXML
+    private void onActionBtnSession(ActionEvent event) {
+    }
     
 }
--- Project_Card/src/main/java/module-info.java
+++ Project_Card/src/main/java/module-info.java
@@ -2,7 +2,9 @@ module cr.ac.una.project_card {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.base;
+    requires MaterialFX;
 
-    opens cr.ac.una.project_card to javafx.fxml;
+    opens cr.ac.una.project_card.controller to javafx.fxml;
+    //opens cr.ac.una.project_card to javafx.fxml;
     exports cr.ac.una.project_card;
 }

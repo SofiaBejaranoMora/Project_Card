5c9ec9bd56adb18ea48ed3b50c6934fdbd28fe3f
AshlyrAMIREZ <ashly.navarro.ramirez@est.una.ac.cr>
2025-05-18 11:43:38 -0600

    Metiendo el Achievements

--- Project_Card/src/main/java/cr/ac/una/project_card/controller/AchievementsController.java
+++ Project_Card/src/main/java/cr/ac/una/project_card/controller/AchievementsController.java
@@ -2,11 +2,21 @@ package cr.ac.una.project_card.controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 
 /** * FXML Controller class * * @author ashly */
 public class AchievementsController  extends Controller  implements Initializable {
 
+    @FXML
+    private Button btnBack;
+
+    @FXML
+    private void onActionBtnBack(ActionEvent event) {
+    }
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
@@ -16,5 +26,5 @@ public class AchievementsController  extends Controller  implements Initializabl
     public void initialize() {
         
     }
-    
+
 }

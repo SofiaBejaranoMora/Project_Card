d21a4e3e280fc79794baed43f1c6affe151856f2
AshlyrAMIREZ <ashly.navarro.ramirez@est.una.ac.cr>
2025-05-18 13:35:00 -0600

    Tab's de Achievements

--- Project_Card/src/main/java/cr/ac/una/project_card/controller/AchievementsController.java
+++ Project_Card/src/main/java/cr/ac/una/project_card/controller/AchievementsController.java
@@ -1,11 +1,13 @@
 package cr.ac.una.project_card.controller;
 
+import cr.ac.una.project_card.util.FlowController;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 /** * FXML Controller class * * @author ashly */
 public class AchievementsController  extends Controller  implements Initializable {
@@ -15,6 +17,7 @@ public class AchievementsController  extends Controller  implements Initializabl
 
     @FXML
     private void onActionBtnBack(ActionEvent event) {
+        FlowController.getInstance().goViewInStage("MenuView", (Stage) btnBack.getScene().getWindow());
     }
     
     @Override

1e8351f9403bdaa60a17c82dbde0230259c080a3
SofiaBejaranoMora <142953533+SofiaBejaranoMora@users.noreply.github.com>
2025-06-07 05:18:05 -0600

    Actualizar columnas

--- Project_Card/src/main/java/cr/ac/una/project_card/service/StackcardxcardService.java
+++ Project_Card/src/main/java/cr/ac/una/project_card/service/StackcardxcardService.java
@@ -6,7 +6,6 @@ package cr.ac.una.project_card.service;
 
 import cr.ac.una.project_card.model.Card;
 import cr.ac.una.project_card.model.CardDto;
-import cr.ac.una.project_card.model.Player;
 import cr.ac.una.project_card.model.Stackcard;
 import cr.ac.una.project_card.model.StackcardDto;
 import cr.ac.una.project_card.model.Stackcardxcard;
@@ -15,6 +14,7 @@ import cr.ac.una.project_card.util.EntityManagerHelper;
 import cr.ac.una.project_card.util.Respuesta;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityTransaction;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -82,7 +82,7 @@ public class StackcardxcardService {
                 for (StackcardxcardDto stackcardxcardDto : stackcardDtoList) {
                     if (stackcardxcardDto.getId() != null && stackcardxcardDto.getId() > 0) {
                         et.rollback();
-                        return new Respuesta(false, "Esta stackcardxCard ya existe: "+ stackcardxcardDto.getId(), "SaveStackcardxCard NoResultException");
+                        return new Respuesta(false, "Esta stackcardxCard ya existe: " + stackcardxcardDto.getId(), "SaveStackcardxCard NoResultException");
                     } else {
                         CardDto cardDto = stackcardxcardDto.getCard();
                         StackcardDto stackcardDto = stackcardxcardDto.getStackCard();
@@ -112,11 +112,77 @@ public class StackcardxcardService {
                 et.commit();// para guardar en pase de datos
                 return new Respuesta(true, " ", " ", "Stackcardxcard", null);
             }
-        }
-        catch (Exception ex) {
+        } catch (Exception ex) {
             et.rollback(); // lo de vuelve como estaba antes del begin si no he hecho commit
             Logger.getLogger(StackcardxcardService.class.getName()).log(Level.SEVERE, "Error guardando la stackcardxCard", ex);
             return new Respuesta(false, "Error guardando la stackcardxCard.", "StackcardxCard " + ex.getMessage());
         }
+    }
+
+    public Respuesta updateStackcardxCardList(List<StackcardxcardDto> stackcardDtoList) {
+        try {
+            et = em.getTransaction();
+            et.begin();
+            if (stackcardDtoList.isEmpty()) {
+                et.rollback();
+                return new Respuesta(false, "La lista stackcardDtoList esta vacia", "SaveStackcardxCard NoResultException");
+            } else {
+                Stackcardxcard stackcardxcard;
+                if (!cleanStackCardLits(stackcardDtoList)) { // se limpean la lista de StackcardxCard que tenga el Stackcard y se devuelve un true si no hubieron errores
+                    et.rollback();
+                    return new Respuesta(false, "No se pudieron limpiar la lista de columnasxcartas de columnas.", "SaveStackcardxCardList NoResultException");
+                }
+                for (StackcardxcardDto stackcardxcardDto : stackcardDtoList) {
+
+                    if (stackcardxcardDto.getId() != null && stackcardxcardDto.getId() > 0) {
+                        StackcardDto stackcardDto = stackcardxcardDto.getStackCard();
+                        stackcardxcard = em.find(Stackcardxcard.class, stackcardxcardDto.getId());
+
+                        if (stackcardxcard == null) { // verifica que se encontrara la entidad de stackcardDto y si no se encontro tira el error
+                            et.rollback();
+                            return new Respuesta(false, "No se pudo encontrar la entidad de stackcardDto: " + stackcardDto.getId(), "SaveStackcardxCardList NoResultException");
+                        }
+                        stackcardxcard.update(stackcardxcardDto);
+
+                        if (stackcardDto != null) { // revisamos que la carta u columan al que se va a relacionar existab
+                            Stackcard stackcard = em.find(Stackcard.class, stackcardDto.getId());
+                            if (stackcard == null) { // revisamos si la columna y la carta se encontraron
+                                et.rollback();
+                                return new Respuesta(false, "No se encontro la entidad de una columna o la carta a la que asociar el stackcardxcard.", "SaveStackcardxCardList NoResultException");
+                            }
+                            stackcard.getStackCardxCards().add(stackcardxcard);
+                            stackcardxcard.setStackCard(stackcard);
+
+                        } else {
+                            et.rollback();
+                            return new Respuesta(false, "La columna o la carta no existe.", "SaveStackcardxCard NoResultException");
+                        }
+                        em.merge(stackcardxcard); // actualiza un registro nuevo;
+
+                    } else {
+                        et.rollback();
+                        return new Respuesta(false, "Esta stackcardxCard no existe: " + stackcardxcardDto.getId(), "SaveStackcardxCard NoResultException");
+                    }
+                }
+            }
+            et.commit();// para guardar en pase de datos
+            return new Respuesta(true, " ", " ", "Stackcardxcard", null);
+        } catch (Exception ex) {
+            et.rollback(); // lo de vuelve como estaba antes del begin si no he hecho commit
+            Logger.getLogger(StackcardxcardService.class.getName()).log(Level.SEVERE, "Error guardando la stackcardxCard", ex);
+            return new Respuesta(false, "Error guardando la stackcardxCard.", "StackcardxCard " + ex.getMessage());
+        }
+    }
+
+    private Boolean cleanStackCardLits(List<StackcardxcardDto> stackcardxcardDtoList) {
+        Stackcardxcard stackcardxcard;
+        for (StackcardxcardDto stackcardxcardDto : stackcardxcardDtoList) {
+            stackcardxcard = em.find(Stackcardxcard.class, stackcardxcardDto.getId());
+            if (stackcardxcard == null) {
+                return false;
+            }
+            stackcardxcard.getStackCard().setStackCardxCards(new ArrayList<>());
+        }
+        return true;
     }
 }
